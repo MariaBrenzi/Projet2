85906b6ed016849d2e5052a30015ea1e9b5e7ebe
MariaBrenzi <135764467+MariaBrenzi@users.noreply.github.com>
2024-03-07 22:49:03 +0100

    Update Streamlit_KickStarter.py

--- Streamlit_KickStarter.py
+++ Streamlit_KickStarter.py
@@ -7,6 +7,8 @@ This is a temporary script file.
 
 import pandas as pd
 import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
